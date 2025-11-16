f63b961e51570235c7d36037b438f5b5f9ddf1b7
carloslevy117-ctrl <carloslevy117@gmail.com>
2025-11-16 14:23:50 -0300

    fazendo

---
+++ repetição.py
@@ -0,0 +1,4 @@
+for i in range(99,251):
+    print(i,"bugs no software, pegue um deles e conserte..." )
+    print("Tecle"   "CTRL-F5\n")
+print("vamos fazer um cafe!")
---
+++ variação do 6.py
@@ -0,0 +1,4 @@
+valor_inicial = int(input("comecando do:"))
+for i in range(valor_inicial,-1,-1):
+    print(i)
+print("lancar!")

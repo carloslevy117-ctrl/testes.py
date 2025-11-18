75c2a01b7771ffc7f057c013b82d045a12fe5d97
carloslevy117-ctrl <carloslevy117@gmail.com>
2025-11-17 23:02:05 -0300

    doidera

---
+++ condicionada.py
@@ -0,0 +1,18 @@
+Renan = float(input("digite a nota do aluno:"))
+Levy = float(input("digite a nota do aluno:"))
+Lucas = float(input("digite a nota do aluno:"))
+if Renan >=60:
+    print("aprovado")
+else:
+    print("reprovado")
+if Levy >=60:
+    print("aprovado")
+else:
+    print("reprovado")
+if Lucas >=60:
+    print("aprovado")
+else:
+    print("reprovado")
+    print("quem foi o aprovado?")
+
+
--- ex9.py
+++
@@ -1,9 +0,0 @@
-maior = None
-for i in range(1,11):
-    num = int(input(f"digite o {i+1}'numero :"))
-
-    if maior is None or num > maior:
-        maior = num
-
-print(f"o maior numero foi: {maior}")
-
